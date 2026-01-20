2272025c1310fba8de9623c398dbfe9cd09faffb
Misa Guti <misaelarista320@gmail.com>
2026-01-20 13:04:40 -0600

    implementacion de carta

--- script.js
+++ script.js
@@ -24,3 +24,27 @@ links.forEach(link => {
     link.classList.add("active");
   });
 });
+
+
+/* ==== CARTA / ANIMACIÓN ==== */
+
+const cartaTexto =
+`Gracias por permitirme compartir contigo un año como pareja
+lleno de muchos momentos buenos y malos, repleto de muchisimas emociones y 
+con muchas sorpresas. Deseo con todo mi corazón seguir compartiendo mi vida contigo, porque eres la persona
+que más amo en este universo y en todos los que existan. Me siento muy afortunado al poderte tener como mi novia, siempre contigo.
+Corazon de melon ❤️`;
+
+let i = 0;
+const speed = 40; // velocidad de tipeo
+function typeLove() {
+  if (i < cartaTexto.length) {
+    document.getElementById("love-text").textContent += cartaTexto.charAt(i);
+    i++;
+    setTimeout(typeLove, speed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  typeLove();
+});
