1b10d6951936d49526430a2ef717b3e5a3d7ea55
Misa Guti <misaelarista320@gmail.com>
2026-01-19 22:45:39 -0600

    correcciones (boton movil)

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuBtn = document.getElementById("menu-btn");    
+const sidebar = document.getElementById("sidebar");
+
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+   menuBtn.classList.toggle("active");
+     menuBtn.textContent = sidebar.classList.contains("open") ? "✕" : "☰";
+});
+
+sidebar.querySelectorAll("a").forEach(a => {
+  a.addEventListener("click", () => {
+    sidebar.classList.remove("open");
+        menuBtn.classList.remove("active");
+  });
+});
+
+const links = document.querySelectorAll("#sidebar a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    links.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
